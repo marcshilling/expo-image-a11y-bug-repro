92e183e9be7571d1206788111b048ad94ed4e7dc
Marc Shilling <marcshilling@gmail.com>
2026-05-20 10:10:53 -0400

    repro

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Pressable } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={() => console.log('pressed')}>
+        <Image
+          source={{ uri: 'https://dummyimage.com/600x400/000/fff&text=Hello+World' }}
+          style={styles.image}
+          contentFit="cover"
+          accessible={false}
+          accessibilityElementsHidden
+        />
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' },
+  image: { width: 300, height: 300 },
 });
